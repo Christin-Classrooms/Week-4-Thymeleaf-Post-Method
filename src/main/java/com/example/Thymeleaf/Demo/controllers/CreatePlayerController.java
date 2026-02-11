4c7859097eb49dd630ba0f5d89d38f6dde97eea5
CL <leungc22@gmail.com>
2026-02-11 13:13:25 -0500

    Lab 2: Implement CreateFighterController with Validation

--- src/main/java/com/example/Thymeleaf/Demo/controllers/CreatePlayerController.java
+++ src/main/java/com/example/Thymeleaf/Demo/controllers/CreatePlayerController.java
@@ -1,14 +1,16 @@
 package com.example.Thymeleaf.Demo.controllers;
 
 
-import com.example.Thymeleaf.Demo.Model.Player;
-import com.example.Thymeleaf.Demo.Service.PlayerService;
-import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.Thymeleaf.Demo.Model.Player;
+import com.example.Thymeleaf.Demo.Service.PlayerService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class CreatePlayerController {
