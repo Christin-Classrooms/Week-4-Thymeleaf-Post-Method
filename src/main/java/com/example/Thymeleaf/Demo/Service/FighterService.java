1d60a9956fdd93f3cda4cf72cee1cc1b34c099b2
Gabriel Hernandez <gabhernandez@hotmail.ca>
2026-02-10 21:47:13 -0500

    Lab 2: Update FighterService

--- src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
+++ src/main/java/com/example/Thymeleaf/Demo/Service/FighterService.java
@@ -1,10 +1,13 @@
 package com.example.Thymeleaf.Demo.Service;
 
+
+import org.springframework.stereotype.Service;
+import com.example.Thymeleaf.Demo.Model.Fighter;
+
 import java.util.ArrayList;
 import java.util.List;
 
-import com.example.Thymeleaf.Demo.Model.Fighter;
-
+@Service
 public class FighterService {
 
     private final List<Fighter> fighters = new ArrayList<>();
