463efe1ef3acb25526e70e8183340642983f9306
Gabriel Hernandez <gabhernandez@hotmail.ca>
2026-02-10 22:45:25 -0500

    Lab 2: Add validation annotations to Fighter model

--- src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
+++ src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
@@ -4,6 +4,8 @@ package com.example.Thymeleaf.Demo.Model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.*;
+
 
 @Data
 @AllArgsConstructor
@@ -11,9 +13,22 @@ import lombok.NoArgsConstructor;
 public class Fighter {
 
     private int id;
+
+    @NotBlank(message = "Name is required")
     private String name;
+
+    @NotNull(message = "Health is required")
+    @Min(value = 1001, message = "Health must be greater than 1000")
+    @Max(value = 1499, message = "Health must be less than 1500")
     private Integer health;
+
+    @NotNull(message = "Damage is required")
+    @DecimalMax(value = "99.99", message = "Damage must be less than 100")
     private Double damage;
+
+    @NotNull(message = "Resistance is required")
+    @DecimalMin(value = "0.0", message = "Resistance must be between 0 and 10")
+    @DecimalMax(value = "10.0", message = "Resistance must be between 0 and 10")
     private Double resistance;
 
 }
