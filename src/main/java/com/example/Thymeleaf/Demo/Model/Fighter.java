ebaf6959c30f356a95b3f4723b6be08f530dbe20
Raj Patel <raj.patel514@gmail.com>
2026-02-11 22:32:23 -0500

    Lab 2: Implement CreateFighterController with Validation

--- src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
+++ src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
@@ -1,6 +1,10 @@
 package com.example.Thymeleaf.Demo.Model;
-
-import jakarta.validation.constraints.*;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +15,23 @@ import lombok.NoArgsConstructor;
 public class Fighter {
 
     private int id;
+    @NotBlank(message = "Name cannot be empty")
     private String name;
+
+    @Min(value = 999, message = "Health must be between 1001 & 1499")
+    @Max(value = 1501, message = "Health must be between 1001 & 1499")
+    @NotNull(message = "Health cannot be empty")
     private Integer health;
-    private Double damage;
+
+    @NotNull(message = "Damage cannot be empty")
+    @Min(value = 1, message = "Damage must be in range 1-99")
+    @Max(value = 99, message = "Damage must be in range 1-99")
+    private Integer damage;
+
+
+    @DecimalMin(value = "0.0", message = "Resistance must be greater than 0.0")
+    @DecimalMax(value = "10.0", message = "Resistance must be less than 10.0")
+    @NotNull(message = "Resistance cannot be empty")
     private Double resistance;
 
 }
