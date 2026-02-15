0e4a25fa4a5ab94f909ed6315461ef582c241dab
Solomon Ozor <n01668239@humber.ca>
2026-02-14 21:52:14 -0500

    Lab 2: Implement CreateFighterController with Validation

--- src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
+++ src/main/java/com/example/Thymeleaf/Demo/Model/Fighter.java
@@ -11,9 +11,22 @@ import lombok.NoArgsConstructor;
 public class Fighter {
 
     private int id;
-    private String name;
-    private Integer health;
-    private Double damage;
-    private Double resistance;
 
+    @NotBlank(message = "fighter name is required")
+    @Size(min = 2, max = 240, message = "fighter name size must be > 2 and <240")
+    private String name;
+
+    @NotNull(message = "health is required")
+    @Min(value = 1001, message = "health must be more than 1000")
+    @Max(value = 1499, message = "health must be less than 1500")
+    private Integer health;
+
+    @NotNull(message = "damage is required")
+    @Max(value = 99, message = "damage must be less than 100")
+    private Integer damage;
+
+    @NotNull(message = "resistance is required")
+    @DecimalMin(value = "0.0", message = "resistance must be between 0 and 10")
+    @DecimalMax(value = "10.0", message = "resistance must be between 0 and 10")
+    private Double resistance;
 }
